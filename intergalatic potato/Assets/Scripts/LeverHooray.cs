25b395b5e29f86b5822225591820b9d69e8da7f7
ottalwal <142910148+ottalwal@users.noreply.github.com>
2025-01-15 16:10:46 -0500

    Fixed camera on starting scene, fixed double player

--- intergalatic potato/Assets/Scripts/LeverHooray.cs
+++ intergalatic potato/Assets/Scripts/LeverHooray.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UIElements;
-using static UnityEditor.Experimental.AssetDatabaseExperimental.AssetDatabaseCounters;
+
 
 public class LeverHooray : MonoBehaviour
 {
